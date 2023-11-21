723a40e6a2137e800333b84f20cea8e07547f82f
kazburrel <oegbosionu@gmail.com>
2023-11-22 00:39:13 +0100

    layout-setup

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,10 @@
 import './bootstrap';
 
 import { createApp } from 'vue' 
-import HomeIndex from './components/Home/Index.vue' 
+import HomeIndex from './components/Home/index.vue' 
+import HomeNavComponent from './components/Home/layouts/NavbarComponent.vue' 
  
 createApp({}) 
     .component('HomeIndex', HomeIndex)
+    .component('HomeNavComponent', HomeNavComponent)
     .mount('#app') 
