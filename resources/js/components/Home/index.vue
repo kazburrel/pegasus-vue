712c7e918b4fd8b325fd7a63e1a4d9ff08331ad6
kazburrel <oegbosionu@gmail.com>
2023-11-24 15:19:33 +0100

    layout-setup

--- resources/js/components/Home/index.vue
+++ resources/js/components/Home/index.vue
@@ -1,3 +1,6 @@
 <template>
-    Table coming soon.
+    <div class="col-12 mt-4 rounded-5 p-4"
+        style="background-image: url('/images/image.png'); background-size: cover; background-position: center; height: 80vh;">
+        scmbjhsfgyfgwhkj <br> <br> <br> wjfgy
+    </div>
 </template>
